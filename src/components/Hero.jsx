a7dedc78e5012a99b02f4a01f21f3f0d19e82617
emmanueluwa <emmanuel@fulodev.com>
2023-12-12 12:17:37 +0000

    adding hero section with animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { BsArrowRight } from "react-icons/bs";
+import { Link } from "react-scroll";
 
 export const Hero = () => {
   return (
@@ -13,7 +15,32 @@ export const Hero = () => {
         <h1 className="md:w-2/3 lg:text-8xl sm:text-6xl text-5xl mb-5 text-white font-bold lg:leading-snug sm:leading-snug leading-snug">
           Mission: Make Things Better
         </h1>
+
+        {/* banner button and text */}
+        <div className="text-white text-lg flex md:flex-row flex-col-reverse md:items-center items-start justify-between gap-8">
+          <div>
+            <a
+              href=""
+              className="inline-flex items-center gap-2 font-semibold hover:text-orange transition-all duration-300 ease-in"
+            >
+              <span>Get a Quote</span>
+              <BsArrowRight />
+            </a>
+          </div>
+          <div className="md:w-1/2">
+            <p className="">
+              We use software to provide solutions to problems.
+            </p>
+          </div>
+        </div>
       </div>
+
+      {/* scroll for service section  */}
+      <Link
+        to="services"
+        smooth={true}
+        className="cs-down_btn md:mt-20 mt-8"
+      ></Link>
     </div>
   );
 };
