ca30d9dd49e8c5f53fb5dd51ef2a31beab5c4186
emmanueluwa <emmanuel@fulodev.com>
2023-12-13 01:09:56 +0000

    adding carousel for projects section

---
+++ src/components/PortfolioSlider.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { Link } from "react-scroll";
+import Slider from "react-slick";
+
+const PortfolioSlider = () => {
+  const projects = [
+    {
+      title: "Pet friendly property renting platform",
+      subtitle: "See Details",
+      url: "https://www.peteine.com/",
+      image: "public/images/peteine.png",
+    },
+    {
+      title: "Connect with pet owners worldwide",
+      subtitle: "See Details",
+      url: "/",
+      image: "public/images/petm8.png",
+    },
+    {
+      title: "Generate dog images with AI",
+      subtitle: "See Details",
+      url: "https://www.creator.fulodev.com/",
+      image: "public/images/icons.png",
+    },
+  ];
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          dots: true,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          dots: true,
+          arrows: false,
+        },
+      },
+    ],
+  };
+
+  return (
+    <Slider {...settings} className="lg:-mx-64 gap-5">
+      {projects.map((item, index) => (
+        <Link
+          key={index}
+          to={index.url || "/"}
+          className={`cs-portfolio cs-bg relative`}
+        >
+          <div className="absolute top-0 left-0 bg-black/50 w-full h-full rounded-2xl"></div>
+          <div>
+            <img src={item.image} alt="" />
+          </div>
+          <div className="absolute top-0 left-0 hover:bg-orange/80 w-full h-full flex items-center justify-center rounded-2xl cursor-pointer">
+            <div className="text-center">
+              <h2 className="text-2xl font-semibold mb-1">{item.title}</h2>
+              <p className="cs-portfolio_subtitle">{item.subtitle}</p>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </Slider>
+  );
+};
+
+export default PortfolioSlider;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import PortfolioSlider from "./PortfolioSlider";
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div>
+      <div className="text-center pb-14 lg:px-24 px-4" id="portfolio">
+        <h4 className="text-gray-200 font-semibold text-2xl">
+          Latest Projects
+        </h4>
+        <h2 className="text-5xl font-bold leading-snug text-white">
+          Explore our most popular projects
+        </h2>
+      </div>
+
+      {/* carousel card */}
+      <div className="lg:px-24 px-4 overflow-hidden">
+        <PortfolioSlider />
+      </div>
+    </div>
+  );
 };
